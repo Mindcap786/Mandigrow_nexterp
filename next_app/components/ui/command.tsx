cc650c0e23ce62c568cd0885235d46a4b1288d46
Mindcap786 <mindcap786@gmail.com>
2026-06-17 14:32:49 +0530

    fix: restore onWheel propagation stop for CommandList to fix scrolling in popovers

--- next_app/components/ui/command.tsx
+++ next_app/components/ui/command.tsx
@@ -63,8 +63,8 @@ const CommandList = React.forwardRef<
     <CommandPrimitive.List
         ref={ref}
         className={cn("max-h-[300px] overflow-y-auto overflow-x-hidden", className)}
-        
-        
+        onWheel={(e) => e.stopPropagation()}
+        onTouchMove={(e) => e.stopPropagation()}
         {...props}
     />
 ))
